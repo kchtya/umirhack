1e6af91913ef419169e5ac5093397cc80fd97f50
kchtya <lonely.sock0@gmail.com>
2025-11-09 11:14:17 +0300

    ошибка

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -76,7 +76,7 @@ body {
 
 /* Подключаем тонкий шрифт Inter */
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap&subset=cyrillic');
+@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap&subset=cyrillic');npm run dev -- --host
 
 /* Тёмная тема (по умолчанию) */
 .theme-dark {
